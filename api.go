66eb10d651e2267ddcca62d0363840dff8393eb9
VHall1 <victorhallpsn@hotmail.com>
2025-09-10 23:38:12 +0100

    implement basic rest api to list transactions

---
+++ api.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type HTTPServer struct {
+	addr string
+	svc  *MonzoClient // TODO: define an interface for this so we're not tied to MonzoClient
+}
+
+func NewHTTPServer(addr string, svc *MonzoClient) *HTTPServer {
+	return &HTTPServer{
+		addr: addr,
+		svc:  svc,
+	}
+}
+
+func (s *HTTPServer) Start() error {
+	handler := http.NewServeMux()
+
+	handler.HandleFunc("GET /transactions", func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
+		defer cancel()
+
+		transactions, err := s.svc.ListTransactions(ctx, r.URL.Query().Get("account_id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		writeJSON(w, http.StatusOK, transactions)
+	})
+
+	return http.ListenAndServe(s.addr, handler)
+}
+
+func writeJSON(w http.ResponseWriter, s int, v any) error {
+	w.WriteHeader(s)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
 	"os"
 )
 
 func main() {
-	ctx := context.Background()
-	c := NewMonzoClient(os.Getenv("MONZO_TOKEN"))
-	transactions, err := c.ListTransactions(ctx, os.Getenv("MONZO_ACCOUNT_ID"))
-	if err != nil {
+	srv := NewHTTPServer(":8080", NewMonzoClient(os.Getenv("MONZO_TOKEN")))
+	if err := srv.Start(); err != nil {
 		log.Fatal(err)
-	}
-	log.Printf("Fetched %d transactions", len(transactions))
-	for _, t := range transactions {
-		log.Printf("%+v", t)
 	}
 }
